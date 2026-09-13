231886b945f5b961f1ed2a105a72c49b8155bc73
Tarhan <tarhanalam023@gmail.com>
2026-09-13 18:05:14 +0600

    Algo Done

---
+++ main.py
@@ -0,0 +1,11 @@
+# Selection Sort Algorithm
+
+def selection_sort(nums: list[int]) -> list[int]:
+    for i in range(len(nums)):
+        smallest_idx = i
+        for j in range(i + 1, len(nums)):
+            if nums[j] < nums[smallest_idx]:
+                smallest_idx = j
+        nums[i], nums[smallest_idx] = nums[smallest_idx], nums[i]
+    return nums
+
